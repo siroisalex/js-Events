aae8675fbe13c4686365907227fbe3b446b2862e
Alexandra Sirois <AlexandraSirois@Alexandras-MacBook-Pro.local>
2017-06-26 13:39:20 -1000

    completed assignment

--- js/app.js
+++ js/app.js
@@ -6,35 +6,72 @@ The function will add the following content inside the paragraph with the id of 
 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
+function moreContent(){
+  var pBox = document.getElementById("more");
+  pBox.innerHTML = bacon;
+}
+
 
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
 
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
 
+function lessContent(){
+  var pLessBox = document.getElementById("less");
+  pLessBox.innerHTML = "";
+}
+
 
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
 
+function zoom(){
+  pBiggieBox = document.getElementById("biggie");
+  pBiggieBox.style.fontSize = "150px";
+}
 
 //4. McDonalds
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
 
 Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
 
+var menuItems = ["french fries," + "" + " salad," + "" + " chicken nuggets"];
+
+function valueMenu(){
+  pMenuBox = document.getElementById("menu");
+  pMenuBox.innerHTML = menuItems;
+}
 
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 20px after clicking on the text.*/
 
+function redFace(){
+  var pDrink = document.getElementById("drink");
+  pDrink.style.color = "red";
+  pDrink.style.fontSize = "20px";
+}
 
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
 
+function showPrice(){
+  var pOreoPrice = document.getElementById("price");
+  pOreoPrice.innerHTML = "$5.55";
+}
 
 //7. Mr. Buttons
 /*Add an Event Listener to the button that will display `myQuote` inside the paragraph with the id of `displayQuote` after the button is clicked.*/
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
+
+var buttonBox = document.getElementById("Benjamin");
+buttonBox.addEventListener("click", showQuote);
+
+function showQuote(){
+  var pQuoteBox = document.getElementById("displayQuote");
+  pQuoteBox.innerHTML = myQuote;
+}
 
 
 //8. Say It again, Randomly
@@ -42,8 +79,35 @@ var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
+var beniButtonBox = document.getElementById("random");
+beniButtonBox.addEventListener("click", randomQuote);
+
+function randomQuote(){
+  var quoteBox = document.getElementById("displayQuotes");
+  var randomQuoteBox = quotes[Math.floor(Math.random()*3)];
+  console.log(randomQuoteBox);
+  quoteBox.innerHTML = randomQuoteBox;
+}
 
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clickig on the button.
 */
+
+var moneyButtonBox = document.getElementById("showHide");
+moneyButtonBox.addEventListener("click", moneySecret);
+
+function moneySecret(){
+  var pShowMoneyBox = document.getElementById("showmoney");
+  if(showmoney.style.display === "none"){
+    showmoney.style.display = "block";
+  }else{
+    showmoney.style.display = "none";
+  }
+}
+
+
+
+
+
+
